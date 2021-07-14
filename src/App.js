88f70c371ca92d30236bd8b9fd13394b13c24331
turekjesse <turekjesse@gmail.com>
2021-07-14 15:27:14 -0400

    language and languages components imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Languages from '../components/Languages'
+import Language from '../components/Language'
 import './App.css';
+
+//COMPONENTS
 
 function App() {
   return (
     <>
     Hello from App.js
+    <Languages/>
+    <Language/>
     </>
   );
 }
