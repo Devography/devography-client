4083e7ff6c9f6a920e178da65299ea618bbd410a
turekjesse <turekjesse@gmail.com>
2021-07-15 18:10:41 -0400

    cardGroups fixed, links to a Lang added & working

--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -1,49 +1,31 @@
-import { CardDeck, Card } from "react-bootstrap";
-import { FaJsSquare } from "react-icons/fa";
+import { CardGroup, Card } from "react-bootstrap";
+import { Link } from 'react-router-dom'
 
-const getLanguages = [
-  {
-    name: "Javascript",
-    author: "Brendan Eich",
-    release_date: "December 4, 1995",
-    logo: <FaJsSquare/>,
-  },
-  {
-    name: "Javascript",
-    author: "Brendan Eich",
-    release_date: "December 4, 1995",
-    logo: <FaJsSquare/>,
-  },
-  {
-    name: "Javascript",
-    author: "Brendan Eich",
-    release_date: "December 4, 1995",
-    logo: <FaJsSquare/>,
-  },
-];
+const Languages = ({match, languages}) => {
 
-const Languages = () => {
   return (
-    <CardDeck>
+    <CardGroup>
       {
-          getLanguages.map((language, idx) => {
+          languages.map((language, idx) => {
             return (
-                <Card key={idx}>
-                    {language.logo}
-                    <Card.Body>
-                    <Card.Title>{language.name}</Card.Title>
-                    <Card.Text>
-                        Author: {language.author}
-                    </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                    <small className="text-muted">Released: {language.release_date}</small>
-                    </Card.Footer>
-                </Card>
+                <Link to={`/languages/${language._id}`} key={idx}>
+                    <Card style={{maxWidth: "200px"}}>
+                        <Card.Img variant="top" src={language.logo}/>
+                        <Card.Body>
+                        <Card.Title>{language.name}</Card.Title>
+                        <Card.Text>
+                            Author: {language.author}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Released: {language.year}</small>
+                        </Card.Footer>
+                    </Card>
+                </Link>
             );
         })
       }
-    </CardDeck>
+    </CardGroup>
   );
 };
 
