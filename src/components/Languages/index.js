53ed345dcb8d924dcd779570269466785d940991
turekjesse <turekjesse@gmail.com>
2021-07-15 12:21:07 -0400

    mock data & card deck sample added

--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -1,7 +1,41 @@
-const Languages = () => {
-    return (
-        <p>Hello from the Languages component</p>
-    )
-}
+import { CardDeck, Card } from "react-bootstrap";
+import { FaJsSquare } from "react-icons/fa";
 
-export default Languages
+const getLanguages = [
+  {
+    name: "Javascript",
+    author: "Brendan Eich",
+    release_date: "December 4, 1995",
+    logo: "<FaJsSquare/>",
+  },
+  {
+      "_id":{"$oid":"60eef0ebf439be7b0862825a"},"name":"Ruby","Author":"Yukihiro \"matz\" Matsumoto","year":"1995","Logo":" https://cdn-images-1.medium.com/max/1200/1*9hd_8qR0CMZ8L0pVbFLjDw.png"},
+  {
+    name: "Javascript",
+    author: "Brendan Eich",
+    release_date: "December 4, 1995",
+    logo: "<FaJsSquare/>",
+  },
+];
+
+const Languages = () => {
+  return (
+    <CardDeck>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardDeck>
+  );
+};
+
+export default Languages;
