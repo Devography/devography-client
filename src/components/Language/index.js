2f776210a277e6dcf9e9c77e2234d4ee4b1e956c
turekjesse <turekjesse@gmail.com>
2021-07-15 12:23:37 -0400

    CreateComment & Comments components imported

--- src/components/Language/index.js
+++ src/components/Language/index.js
@@ -1,6 +1,13 @@
+import CreateComment from "../CreateComment"
+import Comments from "../Comments"
+
 const Language = () => {
     return (
+        <>
         <p>Hello from the Language component</p>
+        <CreateComment/>
+        <Comments/>
+        </>
     )
 }
 
