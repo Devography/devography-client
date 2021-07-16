a058f731eb095fcac234a6cc00af78d3fef6d25d
turekjesse <turekjesse@gmail.com>
2021-07-15 20:55:55 -0400

    getLanguage func complete - data retrieved

--- src/components/Language/index.js
+++ src/components/Language/index.js
@@ -1,7 +1,27 @@
+import {useState, useEffect} from 'react'
 import CreateComment from "../CreateComment"
 import Comments from "../Comments"
 
-const Language = () => {
+const Language = ({match}) => {
+
+    const [language, setLanguage] = useState([])
+    const API_ENDPOINT_ID = `http://localhost:4000/languages/${match.params.id}`
+
+    const getLanguage = async () => {
+        try {
+            const response = await fetch(API_ENDPOINT_ID)
+            const data = await response.json()
+            setLanguage(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect (() => {
+        getLanguage()
+        //eslint-disable-next-line
+    }, [])
+
     return (
         <>
         <p>Hello from the Language component</p>
