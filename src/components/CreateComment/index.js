1b84e3b970fa0fb920bf894ec2d20cda9e47dd1e
trieule50 <trieule50@gmail.com>
2021-07-16 09:47:24 -0400

    Create Comment Functionality Completed

--- src/components/CreateComment/index.js
+++ src/components/CreateComment/index.js
@@ -1,10 +1,26 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 //pass out match from language
-export default function CreateComment(){
+export default function CreateComment({ match }){
+
+const [ language, setLanaguage ] = useState(null)
+
+const getIndividualLanguage = async () => {
+    try{
+        const response = await fetch(`http:localhost:8080/languages${match.params.id}`);
+        const data = response.json();
+        setLanaguage(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+useEffect(()=>{
+    getIndividualLanguage();
+},[])
 
 // Modal state
 const [show, setShow] = useState(false);
@@ -14,40 +30,43 @@ const handleShow = () => setShow(true);
 
 const initialFormValues={
     name:"",
-    body:""
+    body:"",
+    languageId: `${match.params.id}`
 }
 
 const [newComment, setNewComment] = useState(initialFormValues);
 
 const _handleChange = (e) => {
-    setNewComment(prevState =>{
-        return{
-            ...prevState,
-            [e.target.id]: e.target.value
-        }
-    })
+    setNewComment({
+            ...newComment, [e.target.id]: e.target.value}
+    )
 }
 
-// const _handleSubmit = async(e) => {
-//     e.preventDefault();
-//     try{
-//         // const API_ENDPOINT = 'http://localhost/comments
-//         const response = await fetch(API_ENDPOINT, {
-//             method: 'POST',
-//             body: JSON.stringify(newComment),
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
-//         if (response.status === 201) {
-//             getCommentData();
-//             setNewComment(initialFormValues)
-//         }
+//Need to add a refresh in handle submit functionality.
 
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
+const _handleSubmit = async(event) => {
+    event.preventDefault();
+    const COMM_API_ENDPOINT = `http://localhost:8080/comments`
+    try{
+        const response = await fetch(COMM_API_ENDPOINT, {
+            method: 'POST',
+            body: JSON.stringify(newComment),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.status === 201) {
+            getIndividualLanguage();
+            setNewComment(initialFormValues);
+            handleClose();
+        }else{
+            alert("Oops, something went wrong. Try again")
+        }
+
+    }catch(err){
+        console.log(err)
+    }
+}
     return(
         <>
         <h2 style={{textAlign:'center'}}> {"{"} Comments {"}"}</h2>
@@ -60,28 +79,28 @@ const _handleChange = (e) => {
             <Modal.Title>Add a Comment</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form onSubmit={_handleSubmit}>
+                    <Form.Group className="mb-3">
                     <Form.Label>Name:</Form.Label>
-                    <Form.Control type="name" id="name" value={newComment.name} onChange={_handleChange} required/>
+                    <Form.Control type="name" id="name"value={newComment.name} onChange={_handleChange} required/>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Group className="mb-3">
                     <Form.Label>Comment:</Form.Label>
-                    <Form.Control as="textarea" rows={3} id="name" value={newComment.body} onChange={_handleChange} required/>
+                    <Form.Control as="textarea" rows={3} id="body" value={newComment.body} onChange={_handleChange} required/>
                     </Form.Group>
+                    {/* <Button variant="primary" onClick={_handleSubmit}>
+                        Save Comment
+                    </Button> */}
+                    <input type="submit" value="Add Comment"/>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
-            <Button variant="primary" onClick={handleClose}>
-                Save Changes
-            </Button>
+            
             </Modal.Footer>
             </Modal>
         </>
     )
 }
-
-//button value={match.params.id} onClick={_handleSubmit}
