663bb048650a3eccc9ac2518541e76ff6fd9a47c
CrowdedAstronaut <ross@johnrossphillips.com>
2021-07-19 08:39:51 -0500

    Changed docs

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,78 +1,94 @@
-import {useState, useEffect} from 'react'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
-export default function Comments({ match }){
+export default function Comments({ match }) {
+  const [comments, setComments] = useState([]);
 
-    const [comments, setComments] = useState([]);
+  //API call to post comments - will need to pass out match props to get lanaguage ID and pass it to req.body
 
-    //API call to post comments - will need to pass out match props to get lanaguage ID and pass it to req.body
-
-    const getCommentsData = async () => {
-        const LANG_API_ENDPOINT = `http://localhost:4000/languages/${match.params.id}`
-        try{
-            const response = await fetch(LANG_API_ENDPOINT);
-            const data = await response.json();
-            setComments(data.comments)
-        }catch(err){
-            console.log(err)
-        }
+  const getCommentsData = async () => {
+    const LANG_API_ENDPOINT = `http://localhost:4000/languages/${match.params.id}`;
+    try {
+      const response = await fetch(LANG_API_ENDPOINT);
+      const data = await response.json();
+      setComments(data.comments);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(()=>{
+  useEffect(() => {
+    getCommentsData();
+  }, []);
+
+  //function to handle delete
+
+  const _handleDelete = async (e) => {
+    const COMM_API_ENDPOINT = `http://localhost:4000/comments/${e.target.value}`;
+    try {
+      const deleteComment = await fetch(COMM_API_ENDPOINT, {
+        method: "DELETE",
+      });
+      if (deleteComment.status === 204) {
+        console.log("Comment have been deleted");
         getCommentsData();
-    },[])
-
-    //function to handle delete
-
-    const _handleDelete = async (e) =>{
-        const COMM_API_ENDPOINT = `http://localhost:4000/comments/${e.target.value}`
-        try{
-            const deleteComment = await fetch(COMM_API_ENDPOINT, {
-                method: 'DELETE',
-            })
-            if(deleteComment.status === 204){
-                console.log('Comment have been deleted')
-                getCommentsData();
-            }else{
-                alert('Oops, something went wrong!')
-            }
-        }catch(err){
-            console.log(err)
-        }
+      } else {
+        alert("Oops, something went wrong!");
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    if (!comments) {
-        return <p>No comments</p>
-    }
+  if (!comments) {
+    return <p>No comments</p>;
+  }
 
-    return(
-        <Container>
-            {comments.map((comment, i) =>{
-                return(
-                    <div className='comment-container' key={i}>
-                        <div className='comment-items'>
-                            <p><span style={{color:'blue'}}>{comment.name}</span>: {comment.body}</p>
-                            {comment.createdAt === comment.updatedAt ? (
-				                <small>Created at: {new Date(comment.createdAt).toLocaleString()}</small>
-			                    ) : (
-				                <small>Updated at: {new Date(comment.updatedAt).toLocaleString()}</small>
-			                )}     
-                        </div>
-                        <div>
-                            <Button variant="outline-secondary"> 
-                            <Link style={{
-                                    textDecoration:'none',
-                                    color:'rgb(97, 106, 113)'
-                                    }} to={`/languages/${match.params.id}/comments/${comment._id}/edit`}>EDIT</Link>
-                            </Button>
-                            <Button variant="secondary" onClick={_handleDelete} value={comment._id}>DELETE</Button>
-                        </div>
-                    </div>
-                )
-            })}
-        </Container>
-    )
+  return (
+    <Container>
+      {comments.map((comment, i) => {
+        return (
+          <div className="comment-container" key={i}>
+            <div className="comment-items">
+              <p>
+                <span style={{ color: "blue" }}>{comment.name}</span>:{" "}
+                {comment.body}
+              </p>
+              {comment.createdAt === comment.updatedAt ? (
+                <small>
+                  Created at: {new Date(comment.createdAt).toLocaleString()}
+                </small>
+              ) : (
+                <small>
+                  Updated at: {new Date(comment.updatedAt).toLocaleString()}
+                </small>
+              )}
+            </div>
+            <div>
+              <Button variant="outline-secondary">
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "rgb(97, 106, 113)",
+                  }}
+                  to={`/languages/${match.params.id}/comments/${comment._id}/edit`}
+                >
+                  EDIT
+                </Link>
+              </Button>
+              <Button
+                variant="secondary"
+                onClick={_handleDelete}
+                value={comment._id}
+              >
+                DELETE
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+    </Container>
+  );
 }
