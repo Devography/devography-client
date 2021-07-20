e4a3b2311f5bc938810069fbd25a366819bbdd27
turekjesse <turekjesse@gmail.com>
2021-07-19 20:21:12 -0400

    added navbar brand

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,15 +1,29 @@
 import Nav from "react-bootstrap/Nav";
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavHeader() {
   return (
-    <Nav activeKey="/home" className="justify-content-end">
-      <LinkContainer to="/">
-        <Nav.Link>Home</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/languages">
-        <Nav.Link>Languages</Nav.Link>
-      </LinkContainer>
-    </Nav>
+
+    <Navbar sticky="top" expand="lg">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>dev{' { } '}graphy</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/languages">
+            <Nav.Link>Languages</Nav.Link>
+          </LinkContainer>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
   );
 }
